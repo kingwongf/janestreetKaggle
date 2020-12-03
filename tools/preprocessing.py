f5da0e2781fb335cb71cd7008f5d0ba772d2e336
kingf.wong <kingwongf@gmail.com>
2020-12-03 12:17:22 +0000

    tidy_utility_score

--- tools/preprocessing.py
+++ tools/preprocessing.py
@@ -9,39 +9,70 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from scipy import stats
 
 
-def rm_outlier(df, theshold=5):
-	z = np.abs(stats.zscore(df, nan_policy='omit'))
-	return train[(z < threshold).all(axis=1)].reset_index(drop=True)
+def rm_outlier(df, threshold=5):
+    '''
+
+    :param df:
+    :param threshold:
+    :return:
+    '''
+    z = np.abs(stats.zscore(df, nan_policy='omit'))
+    return df[(z < threshold).all(axis=1)].reset_index(drop=True)
 
 def train_test_split(df, int_dt_window=100):
-	'''
-	Gives expanding train, test integer indices, 
-	'''
-	epochs =  range(int_dt_window, max(df['date']) - int_dt_window + 1, int_dt_window )
+    '''
+    Gives expanding train, test integer indices,
+    '''
+    epochs =  range(int_dt_window, max(df['date']) - int_dt_window + 1, int_dt_window )
 
-	return [(df[(df['date'] < idt)].index.values, 
-			 df[(df['date'] >= idt) & 
-			 	   (df['date'] < idt + int_dt_window)].index.values) for idt in epochs]
+    return [(df[(df['date'] < idt)].index.values,
+             df[(df['date'] >= idt) &
+                   (df['date'] < idt + int_dt_window)].index.values) for idt in epochs]
 
 def train_pca(X, n_components):
-	pca = PCA(n_components=n_components).fit(X)
-	return (pca, pca.transform(X))
+    pca = PCA(n_components=n_components).fit(X)
+    return (pca, pca.transform(X))
 
 def norm_weight(train):
-	train['weight'] = train['weight']/ train['weight'].sum()
-	return train
+    train['weight'] = train['weight']/ train['weight'].sum()
+    return train
 
 
 def scaling(X, scaler='MinMaxScaler'):
-	scaler_ = getattr(locals(), scaler)().fit(X)
-	return (scaler, scaler.transform(X))
+    scaler_ = getattr(locals(), scaler)().fit(X)
+    return (scaler, scaler.transform(X))
 
 def utility(dates: np.ndarray, returns: np.ndarray,
             weights: np.ndarray, actions: np.ndarray) -> np.ndarray:
+
     gains = pd.DataFrame({
         'date': dates,
         'gain': weights * returns * actions
     })
+
+    '''
+	utility score of predictions of test dataset 
+    :param dates: test dates in np.array (n,1)
+    :param returns: test 'resp' in np.array (n,1)
+    :param weights: test 'weight in np.array (n,1)
+    :param actions: prediction of test's 'action' in np.array (n,1)
+    :return: utility score of whole test dataset
+    
+    e.g.
+    
+    probs = clf.predict_proba( np.nan_to_num(sc.transform(test_X[feat_cols]), nan=-999) )[:,1] 
+	thresholds = 0.5 + 0.20 * test_weight * (1/ max_weight)
+
+	ind_tw = test_weight.astype(bool).astype(int)
+	test_pred = (probs > thresholds).astype(int)*ind_tw
+
+	print( utility( dates=train[train.date>=375]['date'], 
+				   returns=train[train.date>=375]['resp'], 
+				   weights=test_weight, actions=test_pred
+				   )
+			)
+    '''
+
 
     p = gains.groupby('date').sum().to_numpy()
 
@@ -53,6 +84,6 @@ def utility(dates: np.ndarray, returns: np.ndarray,
 
 
 def mutualInfo(x,y,bXY,norm=False):
-	cXY=np.histogram2d(x,y,bXY)[0] iXY=mutual_info_score(None,None,contingency=cXY) # mutual information if norm:
-	hX=ss.entropy(np.histogram(x,bXY)[0]) # marginal hY=ss.entropy(np.histogram(y,bXY)[0]) # marginal iXY/=min(hX,hY)
-	return iXY
+    cXY=np.histogram2d(x,y,bXY)[0] iXY=mutual_info_score(None,None,contingency=cXY) # mutual information if norm:
+    hX=ss.entropy(np.histogram(x,bXY)[0]) # marginal hY=ss.entropy(np.histogram(y,bXY)[0]) # marginal iXY/=min(hX,hY)
+    return iXY
