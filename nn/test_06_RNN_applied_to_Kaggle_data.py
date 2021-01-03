9ff9c21bc1b15dfbaf380906a5b273fb547fbfea
FabIT <40968674+faberAndro@users.noreply.github.com>
2021-01-03 14:30:48 +0000

    Extracted features stats. Saved their plot

--- nn/test_06_RNN_applied_to_Kaggle_data.py
+++ nn/test_06_RNN_applied_to_Kaggle_data.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 import numpy as np
 import janestreetKaggle.tools.big_csv_reader as bcr
-import tensorflow as tf
 from tensorflow import keras
 
 # JUST A COPY OF TEST_05 FOR NOW. TO BE DEVELOPED
@@ -13,44 +12,76 @@ from tensorflow import keras
 # EACH SEQUENCE IS A SEQUENCE OF N.5 "10-ROW-VECTORS"
 
 
-N_TRAIN = 10000
-N_TEST = 100
+N_TRAIN = 5000
+N_TEST = 1000
+
+# LOAD PART OF KAGGLE DATA
+data = bcr.read_big_csv(filepath=bcr.file, n=N_TRAIN + N_TEST)
 
 
-def generate_hot_vectors(array: np.array) -> np.array:
-    return tf.keras.utils.to_categorical(array, num_classes=10)
+# DROP NAN COLUMNS
+nan_col = [0, 7, 8, 11, 12, 17, 18, 21, 22, 27, 28, 31, 31, 55, 72, 74, 78, 80, 84, 86, 90, 92, 96, 98, 102,
+           104, 108, 110, 114, 116, 120, 121]
+nan_columns = ['date'] + ['feature_' + str(x) for x in nan_col]
+data = data.drop(columns=nan_columns, axis=1)
+nc = data.shape[1]
 
+# GENERATE TRAIN SET:
+# TRANSFORM NOW EACH ROW IN A "(138-5)LENGHT_ONE-HOT-VECTOR" (maybe suppress "weight=0"-rows?)
+# 1. timesteps will be 500 for each input.
+# 2. gap between each input will be 50
+# 3. number of samples will be 20000/50 = 400
+# 4. output will be softmax, containing resp_{1, 2, 3, 4} and resp, each one multiplied by the weight
+# so, the input shape to train the RNN will be: shape=(400, 500, 133)
+# let's try first with n=2000 -> so: shape = (40, 500, 133)
+c = list(data.columns)
+y_columns = ['resp', 'resp_1', 'resp_2', 'resp_3', 'resp_4']
+x_columns = sorted(list(set(c) - set(y_columns)))
+y_data = data[y_columns].multiply(data['weight'], axis='index')
+x_data = data[x_columns]
+# delete now raw data to save memory
+training_sequence = np.zeros((40, 500-1, nc-5))
+training_y_hat = np.zeros((40, 5))
+for i in range(40):
+    print('sequence', i)
+    current_sequence = x_data.iloc[40*i: 40*i+500-1]
+    current_resp = y_data.iloc[40*i+500]
+    training_sequence[i] = current_sequence
+    training_y_hat[i] = current_resp
+# add variable lenght sampling procedure
+# remove Nan in a better way
+# try now to mirror the score into the metric: as close as possible...
+# find max and min in the features
+# find correlation in the features and extract the only one useful
+# find how many nans the feature data have
 
-def generate_sequences(n=N_TRAIN):
-    np.random.seed(3)
-    start = np.random.randint(10, size=n)
-    sequence = np.zeros((n, 5))
-    for i, x in enumerate(start):
-        sequence[i] = np.arange(x, x + 5) % 10
-    return sequence.astype(int)
-
-
-# GENERATE TRAIN SET
-raw_inputs = generate_sequences()
-inputs = generate_hot_vectors(raw_inputs)
-raw_y_hats = ((raw_inputs[:, -1] + 1) % 10)
-y_hats = generate_hot_vectors(raw_y_hats)
-
-# GENERATE TEST SET
-raw_t_inputs = generate_sequences(n=N_TEST)
-t_inputs = generate_hot_vectors(raw_t_inputs)
-raw_t_y_hats = ((raw_t_inputs[:, -1] + 1) % 10)
-t_y_hats = generate_hot_vectors(raw_t_y_hats)
+# GENERATE THE TEST SET
+test_sequence = np.zeros((10, 500-1, nc-5))
+test_y_hat = np.zeros((10, 5))
+for i in range(10):
+    print('sequence', i)
+    current_sequence = x_data.iloc[N_TEST + 10*i: N_TEST + 10*i+500-1]
+    current_resp = y_data.iloc[N_TEST + 10*i+500]
+    test_sequence[i] = current_sequence
+    test_y_hat[i] = current_resp
 
 # BUILD THE MODEL (here each input is a 5-element sequence. Next step: feed variable-lenght sequences)
 model_1 = keras.Sequential()
-model_1.add(keras.layers.LSTM(64, input_shape=(5, 10)))
-model_1.add(keras.layers.Dense(10, activation='softmax'))
+model_1.add(keras.layers.LSTM(128, input_shape=(500, nc-5)))
+model_1.add(keras.layers.Dense(5, activation='softmax'))
 model_1.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model_1.fit(inputs, y_hats, epochs=3)
+model_1.fit(training_sequence, training_y_hat, epochs=3)
+model_1.summary()
+
+# I need to insert the one-hot vector encoding for the result?
+# I NEED TO NORMALIZE FIRST THE FEATURES, TURNING TO 1-HOT-VECTORS WHERE EACH VECTOR-COMPONENT IS BETWEEN 0 AND 1
+# FIRST STRATEGY: encode each resp{1..5} to a one-hot-vector (many dimensions!!) and then have a softmax
+# SECOND STRATEGY(BEST?): have a sigmoid for each of the 5 output neurons, and encode the resp{1..5} to an interval [0..1]
+# In fact, maybe I don't need a softmax: just 5 neurons with a sigmoid
+
 
 # PREDICT AND EVALUATE ON THE TEST SET
-r = model_1.predict(t_inputs)
-result = r.argmax(axis=1)
-n_right_answers = (raw_t_y_hats == result).sum()/N_TEST
-print('accuracy on test set: %.2f %%' % (n_right_answers*100))
+r = model_1.predict(test_sequence)
+# result = r.argmax(axis=1)
+# n_right_answers = (raw_t_y_hats == result).sum()/N_TEST
+# print('accuracy on test set: %.2f %%' % (n_right_answers*100))
--- tools/big_csv_reader.py
+++ tools/big_csv_reader.py
@@ -1,18 +1,35 @@
 # coding=utf-8
 import pandas as pd
 import os
+import time
 
 
-# number of rows found: 2390490 (more than 2 millions)
+# number of rows found: 2390491 (more than 2 millions)
 # number of days : 500
 # number of records per day: 4781 ca.
 
+file = '../jane-street-market-prediction/train.csv'
+file_lenght = 2390490
 
-def read_big_csv(filepath: str, s: int = None, n: int = None) -> pd.DataFrame:
-    if n is not None:
-        rows = pd.read_csv(filepath, header=0, nrows=n)
+
+def assess_time_to_load(filepath: str = file, n: int = 0):
+    t0 = time.time()
+    print('reading %d records' % n)
+    read_big_csv(filepath=filepath, n=n)
+    t1 = time.time()
+    print('read in %f seconds' % (t1 - t0))
+
+
+def read_big_csv(filepath: str = file, s: int = None, n: int = None) -> pd.DataFrame:
+    print('loading records')
+    t0 = time.time()
+    if s is not None or n is not None:
+        rows_to_skip = list(range(0, s)) + list(range(s+n, file_lenght))
+        rows = pd.read_csv(filepath, header=0, skiprows=rows_to_skip)
     else:
-        rows = pd.read_csv(filepath, header=0, skiprows=s)
+        rows = pd.read_csv(filepath, header=0)
+    t1 = time.time()
+    print('loaded in %f seconds' % (t1-t0))
     return rows
 
 
@@ -27,11 +44,31 @@ def count_rows(filepath: str):
     return lines
 
 
-file = '../jane-street-market-prediction/train.csv'
+def make_stats(n: int = None) -> (pd.DataFrame, int):
+    stats = {}
+    data = read_big_csv(n=n)
+    stats['minima'] = data.min()
+    stats['maxima'] = data.max()
+    stats['average'] = data.mean()
+    stats['variance'] = data.var()
+    number_of_records = len(data)
+    stats_df = pd.DataFrame(data=stats)
+    stats_df.drop(['date', 'ts_id'])
+    return stats_df, number_of_records
+
+
+def plot_feature_stats():
+    stats = pd.read_csv(r'C:\Kaggle-King\janestreetKaggle\tools\feature_stats.csv', index_col=0)
+    stats.plot()
+
+
 os.chdir(r'C:\Kaggle-King\janestreetKaggle')
 
-if __name__ == '__main__':
-    r = count_rows(filepath=file)
-    view_top = read_big_csv(filepath=file, n=100)
-    # view_bottom = read_big_csv(filepath=file, s=2390400)
 
+if __name__ == '__main__':
+    # r = count_rows(filepath=file)
+    # view_top = read_big_csv(filepath=file, n=100)
+    # view_bottom = read_big_csv(filepath=file, s=2390400)
+    dataf, nr = make_stats()
+    dataf.to_csv(r'C:\Kaggle-King\janestreetKaggle\tools\feature_stats.csv')
+    print('n records = ', nr)
