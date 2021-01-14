57dada78bb9c77312177c638a116e52931b73e45
FabIT <40968674+faberAndro@users.noreply.github.com>
2021-01-14 20:49:30 +0000

    RNN_06 code cleaned out.

--- nn/test_06_RNN_applied_to_Kaggle_data.py
+++ nn/test_06_RNN_applied_to_Kaggle_data.py
@@ -1,22 +1,33 @@
-# todo: correlate the results (resp) to the very high_variance_features
-# todo: how to normalize data that will come in the future? Which will be then minima and maxima?
-
 # coding=utf-8
+
+# todo: CHECK MORE CAREFULLY IF USING FIT IN INCREMENTAL STEPS IS REALLY WORKING
+#  correlate the results (resp) to the very high_variance_features ?
+#  how to normalize data that will come in the future? Which will be then minima and maxima?
+#  worth to suppress rows with 0-weight in a RNN?
+#  further simplification to be done aggregating features in tags?
+#  worth to delete raw data after each batch, to save memory?
+#  worth to find out how many 'nans' in the feature data?
+#  introduce a dev set
+#  introduce a final layer trasforming the ouputs back to "resp"
+
 import numpy as np
 import pandas as pd
 import janestreetKaggle.tools.big_csv_reader as bcr
 from tensorflow import keras
 import os
 
-test = True
-if test:
+rnn_to_use = r'./nn/saved_models/RNN_06_test_3'
+train, predict = False, True
+use_test_size = True
+
+if use_test_size:
     HOT_VECTOR_SIZE = 84
-    BATCH_SIZE = 5000
+    BATCH_SIZE = 100000     # 5000
     MINIMUM_SEQUENCE_LENGHT, MAXIMUM_SEQUENCE_LENGHT = (150, 500)
-    N_BATCH_SAMPLES = 50
-    N_LENGHTS = 10
-    N_RECORDS = 6000
-    N_TRAIN = 5000
+    N_BATCH_SAMPLES = 2000      # 50
+    N_LENGHTS = 50      # 10
+    N_RECORDS = 200000  # 6000
+    N_TRAIN = 190000     # 5000
     N_TEST = N_RECORDS - N_TRAIN
 else:
     HOT_VECTOR_SIZE = 84
@@ -27,7 +38,6 @@ else:
     N_RECORDS = 2390490
     N_TRAIN = 2300000
     N_TEST = N_RECORDS - N_TRAIN
-# better dividing the training set in aliquot parts
 y_columns = ['resp', 'resp_1', 'resp_2', 'resp_3', 'resp_4']
 
 
@@ -40,24 +50,6 @@ def build_the_model():
     return model_1
 
 
-def convert_back_pred_y_hat_to_prob_hat_inputlike(w_df: np.array, y_df: np.array):
-    w_stats = pd.read_csv(r'C:\Kaggle-King\janestreetKaggle\tools\weighted_responses_stats.csv', index_col=0)       # load the file with the wegighted minima & maxima
-    w_resp_min, w_resp_max = w_stats.loc['resp']['minima'], w_stats.loc['resp']['maxima']
-    a = 1 / (w_resp_max - w_resp_min)
-    b = -w_resp_min * a
-    w_original_resp = (y_df - b)/a
-    # w_original_resp = y_df.apply(lambda z: z/a - b/a)
-
-    stats = pd.read_csv(r'C:\Kaggle-King\janestreetKaggle\tools\feature_stats.csv', index_col=0)        # load the file with the minima & maxima
-    w_min, w_max = stats.loc['weight']['minima'], stats.loc['weight']['maxima']
-    a = 1 / (w_max - w_min)
-    b = -w_min * a
-    w_original = (w_df - b)/a
-    original_resp = w_original_resp / w_original
-
-    return w_original, original_resp
-
-
 def load_data_set(start: int = None, n: int = N_TRAIN + N_TEST):
     data = bcr.read_big_csv(filepath=bcr.file, s=start, n=n)
     c = list(data.columns)
@@ -65,14 +57,6 @@ def load_data_set(start: int = None, n: int = N_TRAIN + N_TEST):
     y_data = data[y_columns]
     x_data = data[x_columns]
     return x_data, y_data
-
-
-def _suppress_weight_0_rows():
-    return
-
-
-def _aggregate_features_in_tags():
-    return
 
 
 def suppress_correlated_features(df: pd.DataFrame):
@@ -92,13 +76,18 @@ def fill_nan(df: pd.DataFrame):
     return df
 
 
-def _drop_nan_columns(df):
-    nan_col = [0, 7, 8, 11, 12, 17, 18, 21, 22, 27, 28, 31, 31, 55, 72, 74, 78, 80, 84, 86, 90, 92, 96, 98, 102,
-               104, 108, 110, 114, 116, 120, 121]
-    nan_columns = ['date'] + ['feature_' + str(x) for x in nan_col]
-    data = df.drop(columns=nan_columns, axis=1)
-    nc = data.shape[1]
-    return
+def preprocess_loaded_dataset(s, n):
+    print('LOADING... ', end='')
+    x, y = load_data_set(start=s, n=n)
+    print('PURGING... ', end='')
+    x = suppress_correlated_features(x)
+    x = fill_nan(x)
+    print('WEIGHTING Y HATS... ', end='')
+    y = y.multiply(x['weight'], axis='index')  # weight all the resp and consider them as the effective y_hats
+    print('NORMALISING NOW X AND Y_HATS')
+    x = normalise_data(x, r'C:\Kaggle-King\janestreetKaggle\tools\feature_stats.csv')
+    y = normalise_data(y, r'C:\Kaggle-King\janestreetKaggle\tools\weighted_responses_stats.csv')
+    return x, y
 
 
 def normalise_data(df: pd.DataFrame, n_filename: str):
@@ -120,15 +109,14 @@ def prepare_one_hot_vectors_input_set(df_x: pd.DataFrame, df_y: pd.DataFrame,
     return tracking, x_training_sample, y_hat
 
 
-def prepare_hot_vectors_input_set(df_x: pd.DataFrame, df_y: pd.DataFrame,
-                                  start_points: np.array, sample_size: int,
-                                  test_mode=False):
+def prepare_multiple_hot_vectors_input_set(df_x: pd.DataFrame, df_y: pd.DataFrame,
+                                           start_points: np.array, sample_size: int):
     number_of_samples = len(start_points)
     x_training_set = np.zeros((number_of_samples, sample_size, df_x.shape[1]))
     y_training_set = np.zeros((number_of_samples, df_y.shape[1]))
-    for i in range(number_of_samples):
+    for p in range(number_of_samples):
         _, one_input_sequence, y_hat = prepare_one_hot_vectors_input_set(df_x=df_x, df_y=df_y,
-                                                                      sample_size=sample_size, start=start_points[i])
+                                                                         sample_size=sample_size, start=start_points[p])
         x_training_set[i], y_training_set[i] = one_input_sequence, y_hat
     return x_training_set, y_training_set
 
@@ -142,15 +130,17 @@ def prepare_full_training_set(df_x: pd.DataFrame, df_y: pd.DataFrame,
                                       size=number_of_sample_lenghts,
                                       replace=False)
     start_points = []
-    for i in sample_lenghts:
-        start_points_segment = np.random.choice(range(len(df_x)-i),
+    for lenght in sample_lenghts:
+        start_points_segment = np.random.choice(range(len(df_x)-lenght),
                                                 size=number_of_samples_per_lenght,
                                                 replace=False)
         start_points.extend(start_points_segment)
     start_points = np.asarray(start_points)
-    for i in range(number_of_sample_lenghts):
-        start_points_chunk = start_points[number_of_samples_per_lenght*i: number_of_samples_per_lenght*(i+1)]
-        x_training_chunk, y_training_chunk = prepare_hot_vectors_input_set(df_x=df_x, df_y=df_y, start_points=start_points_chunk, sample_size=sample_lenghts[i])
+    for n_l in range(number_of_sample_lenghts):
+        start_points_chunk = start_points[number_of_samples_per_lenght*i: number_of_samples_per_lenght*(n_l + 1)]
+        x_training_chunk, y_training_chunk = prepare_multiple_hot_vectors_input_set(df_x=df_x, df_y=df_y, 
+                                                                                    start_points=start_points_chunk, 
+                                                                                    sample_size=sample_lenghts[n_l])
         full_x_training_set.append(x_training_chunk)
         full_y_training_set.append(y_training_chunk)
     return full_x_training_set, full_y_training_set
@@ -160,115 +150,63 @@ def prepare_full_test_set(df_x: pd.DataFrame, df_y: pd.DataFrame):
     print('ORGANISING DATA IN APPROPRIATE SETS... ')
     test_list_x, test_list_y = [], []
     test_size = len(df_x)
-    for i in range(test_size - MINIMUM_SEQUENCE_LENGHT):
-        sample_size = np.random.randint(MINIMUM_SEQUENCE_LENGHT, min(MAXIMUM_SEQUENCE_LENGHT, test_size - i))
-        x__test, y__test = prepare_hot_vectors_input_set(df_x=df_x, df_y=df_y,
-                                                         sample_size=sample_size,
-                                                         start_points=[i])
+    for p in range(test_size - MINIMUM_SEQUENCE_LENGHT):
+        sample_size = np.random.randint(MINIMUM_SEQUENCE_LENGHT, min(MAXIMUM_SEQUENCE_LENGHT, test_size - p))
+        x__test, y__test = prepare_multiple_hot_vectors_input_set(df_x=df_x, df_y=df_y,
+                                                                  sample_size=sample_size,
+                                                                  start_points=[i])
         test_list_x.append(x__test)
         test_list_y.append(y__test)
     return test_list_x, test_list_y
 
 
-def fully_prepare_data_sets(s, n):
-    print('LOADING... ', end='')
-    x, y = load_data_set(start=s, n=n)
-    print('PURGING... ', end='')
-    x = suppress_correlated_features(x)
-    x = fill_nan(x)
-    print('WEIGHTING Y HATS... ', end='')
-    y = y.multiply(x['weight'], axis='index')  # weight all the resp and consider them as the effective y_hats
-    print('NORMALISING NOW X AND Y_HATS')
-    x = normalise_data(x, r'C:\Kaggle-King\janestreetKaggle\tools\feature_stats.csv')
-    y = normalise_data(y, r'C:\Kaggle-King\janestreetKaggle\tools\weighted_responses_stats.csv')
-    return x, y
+def convert_back_pred_y_hat_to_prob_hat_inputlike(w_df: np.array, y_df: np.array):
+    w_stats = pd.read_csv(r'C:\Kaggle-King\janestreetKaggle\tools\weighted_responses_stats.csv', index_col=0)       # load the file with the wegighted minima & maxima
+    w_resp_min, w_resp_max = w_stats.loc['resp']['minima'], w_stats.loc['resp']['maxima']
+    a = 1 / (w_resp_max - w_resp_min)
+    b = -w_resp_min * a
+    w_original_resp = (y_df - b)/a
 
+    stats = pd.read_csv(r'C:\Kaggle-King\janestreetKaggle\tools\feature_stats.csv', index_col=0)        # load the file with the minima & maxima
+    w_min, w_max = stats.loc['weight']['minima'], stats.loc['weight']['maxima']
+    a = 1 / (w_max - w_min)
+    b = -w_min * a
+    w_original = (w_df - b)/a
+    original_resp = w_original_resp / w_original
 
-def predict_and_evaluate(model_1, test_sequence):
-    r = model_1.predict(test_sequence)
-    # result = r.argmax(axis=1)
-    # n_right_answers = (raw_t_y_hats == result).sum()/N_TEST
-    # print('accuracy on test set: %.2f %%' % (n_right_answers*100))
-
-
-def compute_scores(edge_0: int, edge_1: int, actions):
-    results = y_data[['resp', 'weight']].iloc[edge_0:edge_1]
-    results['action'] = actions
-    current_score = score(r=results)
-    results['action'] = (results['resp'] > 0)*1
-    max_possible_score = score(r=results)
-    print('max score:', max_possible_score, ' -- this score:', current_score, ' -- %:', current_score/max_possible_score*100)
-    return current_score, max_possible_score
-
-
-def score(r):
-    s = sum(r.prod(axis=1))  # to be refined by date
-    p = np.array([s])
-    t = p.sum() / np.sqrt((p * p).sum()) * np.sqrt(250 / len(p))
-    u = min(max(0, t), 6) * p.sum()
-    return u
-
-
-# TRANSFORM NOW EACH ROW IN A "(138-5)LENGHT_ONE-HOT-VECTOR" (maybe suppress "weight=0"-rows?)
-# 1. timesteps will be 500 for each input.
-# 2. gap between each input will be 50
-# 3. number of samples will be 20000/50 = 400
-# 4. output will be softmax, containing resp_{1, 2, 3, 4} and resp, each one multiplied by the weight
-# so, the input shape to train the RNN will be: shape=(400, 500, 133)
-# let's try first with n=2000 -> so: shape = (40, 500, 133)
-
-# delete now raw data to save memory
-
-# add variable lenght sampling procedure - DONE
-# remove Nan in a better way
-# try now to mirror the score into the metric: as close as possible...
-# find max and min in the features
-# find correlation in the features and extract the only one useful
-# find how many nans the feature data have
-
-# I need to insert the one-hot vector encoding for the result?
-# I NEED TO NORMALIZE FIRST THE FEATURES, TURNING TO 1-HOT-VECTORS WHERE EACH VECTOR-COMPONENT IS BETWEEN 0 AND 1
-# FIRST STRATEGY: encode each resp{1..5} to a one-hot-vector (many dimensions!!) and then have a softmax
-# SECOND STRATEGY(BEST?): have a sigmoid for each of the 5 output neurons, and encode the resp{1..5} to an interval [0..1]
-# In fact, maybe I don't need a softmax: just 5 neurons with a sigmoid
+    return w_original, original_resp
 
 
 if __name__ == '__main__':
-    predict = True
-    train = False
-    inversion_test = True
 
     if train:
         np.random.seed(3)          # ensures the repeatability of the training
         model = build_the_model()
         number_of_batches = N_TRAIN // BATCH_SIZE + bool(N_TRAIN % BATCH_SIZE)
-        for b in range(number_of_batches):
-            print('TRAINING NOW BATCH %d / %d' % (b, number_of_batches))
-            s = b*BATCH_SIZE
-            n = min(BATCH_SIZE, N_RECORDS - s)
-            x, y = fully_prepare_data_sets(s=s, n=n)
-            x_train, y_train = prepare_full_training_set(df_x=x, df_y=y,
+        for batch in range(number_of_batches):
+            print('TRAINING NOW BATCH %d / %d' % (batch, number_of_batches))
+            start_point = batch*BATCH_SIZE
+            n_records = min(BATCH_SIZE, N_RECORDS - start_point)
+            x0, y0 = preprocess_loaded_dataset(s=start_point, n=n_records)
+            x_train, y_train = prepare_full_training_set(df_x=x0, df_y=y0,
                                                          number_of_samples_per_lenght=N_BATCH_SAMPLES,
                                                          number_of_sample_lenghts=N_LENGHTS)
             print('TRAINING')
-            model.fit(x_train[b], y_train[b])       # WARNING!!! IT SEEMS FIT ALONE CANNOT BE USED TO DO INCREMENTAL TRAINING STEPS
+            model.fit(x_train[batch], y_train[batch])       # WARNING!!! NOT SURE IF THE 'FIT' ALONE CAN BE USED TO DO INCREMENTAL TRAINING STEPS
         print('SAVING')
         filename, i_f = r'./nn/saved_models/RNN_06_test_0', 0
         while os.path.exists(filename):
             i_f += 1
             filename = r'./nn/saved_models/RNN_06_test_' + str(i_f)
         model.save(filename)
-        print('EVALUATING ON THE TEST SET ')
-        # NOW TEST THE MODEL OVER THE TEST SET
-        print('EVALUATING THE KAGGLE SCORE ON THE TRAIN AND TEST SETS ')
         print('PROCESS ENDED')
 
     if predict:
         model = keras.models.load_model(r'./nn/saved_models/RNN_06_test_2')
         # PREPARE THE TEST SET:
-        s, n = 6000, 1000      # move 0 to 5000
-        x, y = fully_prepare_data_sets(s=s, n=n)
-        x_test, y_test = prepare_full_test_set(df_x=x, df_y=y)
+        s_test, n_test = N_TRAIN, N_TEST
+        x0, y0 = preprocess_loaded_dataset(s=s_test, n=n_test)
+        x_test, y_test = prepare_full_test_set(df_x=x0, df_y=y0)
         y_pred = []
         print('prediction N#:')
         for i in range(len(x_test)):
@@ -279,10 +217,9 @@ if __name__ == '__main__':
         outcome = m.result().numpy()
         print('mean squared error:', outcome)
         # EVALUATE KAGGLE SCORE ON THE TEST SET:
-        # the following can also probably be done more efficiently with Numpy (maybe using a mask)
-        x_originals, y_originals = load_data_set(n=n)
+        x_originals, y_originals = load_data_set(n=n_test)
         y_hat_pos = [element.shape[1]+i-1 for i, element in enumerate(x_test)]
-        resps_original = np.asarray([y_originals.iloc[p]['resp'] for p in y_hat_pos])
+        resps_original = np.asarray([y_originals.iloc[p]['resp'] for p in y_hat_pos])     # using a numpy mask instead of a generator?
 
         weights = np.asarray([z[0][-1][0] for z in x_test])
         y_used_for_test = np.asarray([z[0][0] for z in y_test])
@@ -300,24 +237,3 @@ if __name__ == '__main__':
         unique_dates_number = len(set(x_originals['date']))
         t = probabilities_sum/probabilities_var * np.sqrt(250/unique_dates_number)
         u = min(max(t, 0), 6)*probabilities_sum
-
-        # there is something strange here above: not quite the right back-algorithm... needs to be corrected.
-        # Let's try to see what happens with y_test in place of y_pred
-        # NEED FIRST TO CORRECT THE PREDICTION IN A WAY THAT, WHEN WEIGHT IS 0, THE PREDICTION IS 0.
-
-        # 1. load all proper weights
-        # 2. transform back y_pred: from [w * resps_{}] to [resps_{}] (only for the simple "RESP")
-        # Do we need all that?? We can simply do, instead: new_resp (=weight*resp) * (reverted_prediction > 0)
-        # first, compute the reverted prediction == 'DE-NORMALISE' data.
-        # load from stats the resp min and max and invert the normalization.
-
-# todo: the y_hats must be the probabilities. SO:
-#  DONE - 1. built the "prob-y-hat-set", and normalise them to [0-1]
-#  DONE - 2. train and evaluate against these y-hats
-#  2b. resolve the problem of training the network in bits and pieces. Is it doing incrementally?
-#  3. evaluate the kaggle-score on the test set: transform back y_hat to resps and also transform back weights. Do not forget to re-include the date.
-#  4. evaluate the kaggle-score on the training set, according to the same methodology
-#  5. introduce a dev set
-#  6. introduce a final layer trasforming the ouputs back to "resp"
-
-
